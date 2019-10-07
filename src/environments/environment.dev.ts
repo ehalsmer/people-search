043f51ee605e7623cd6e1957a7d641717d384cfd
Eleasah Halsmer <eleasahjh@gmail.com>
2019-10-07 16:49:25 -0600

    Pruning, removed angular app, added test function for serverless

--- src/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  API_URL: "https://dev.search.connectourkids.org",
-  APP_URL: "https://dev.search.connectourkids.org",
-  AUTH0_CLIENT_ID: "JUR6JVt9N4sn8bHrSXR818rwrj91hsTb",
-  AUTH0_AUDIENCE: "https://dev.search.connectourkids.org/api/",
-  APP_NAME: "People Search DEV"
-};
--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: "https://search.connectourkids.org",
-  APP_URL: "https://search.connectourkids.org",
-  AUTH0_CLIENT_ID: "i5NGofkl7Z4jEexA7YTbasrLT8pcKLDU",
-  AUTH0_AUDIENCE: "https://search.connectourkids.org/api/",
-  APP_NAME: "People Search by Connect Our Kids"
-};
